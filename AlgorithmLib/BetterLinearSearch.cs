64791ba4c4c5812783b1bc3a7470445af8a7aebb
Benjaminatedu <91627659+Benjaminatedu@users.noreply.github.com>
2023-09-16 09:45:33 -0600

    Completed assignment.

--- AlgorithmLib/BetterLinearSearch.cs
+++ AlgorithmLib/BetterLinearSearch.cs
@@ -4,7 +4,18 @@ public static class BetterLinearSearch
 {
     public static int Search(List<IComparable> data, IComparable target)
     {
-        // ADD CODE HERE AND FIX RETURN STATEMENT
-        return -1;
+        // Set result to default (-1).
+        var result = -1;
+        
+        // Iterate through given list 'data'
+        for (int i = 0; i < data.Count; i++)
+        {
+            // Compare elements. If the element matches the target, assign 'result' to the index value.
+            if (data[i].Equals(target))
+            {
+                result = i;
+            }
+        }
+        return result;
     }
 }
